3d590b3f89a42dc2d7cfeed817656d9312a433f5
shy_azusa <shy.azusa@gmail.com>
2018-09-09 10:49:47 +0900

    Add option page

--- src/js/options.coffee
+++ src/js/options.coffee
@@ -1,8 +1,75 @@
 $ ->
-  chrome.storage.local.get (item) ->
-    if item.rids
-      $('#rids').val(item.rids)
+  addBrTag = ->
+    roomIdLength = $('.roomId').length
+    $('<br>').appendTo "#roomId_#{roomIdLength}"
 
-  $('#save').on 'click', ->
-    chrome.storage.local.set {rids: $('#rids').val()}, ->
-      console.log('save?')
+  addAddButton = ->
+    $ '<input>',
+      id: 'add',
+      type: 'button'
+      value: 'Add'
+    .appendTo '#mainSetting'
+
+  addSaveButton = ->
+    $ '<input>',
+      id: 'save',
+      type: 'button'
+      value: 'Save'
+    .appendTo '#mainSetting'
+
+  addRemoveButton = (roomIdLength) ->
+    $ '<input>',
+      class: 'delete'
+      type: 'button'
+      "data-id": roomIdLength
+      value: 'Delete'
+    .appendTo "#roomId_#{roomIdLength}"
+
+  addRoomId = (rid) ->
+    roomIdLength = $('.roomId').length
+    $ '<div>',
+      id: "roomId_#{roomIdLength}"
+      class: 'roomId'
+    .appendTo '#roomIds'
+    if rid
+      $ '<input>',
+        class: "roomId #{rid}"
+        type: 'text'
+        value: rid
+      .appendTo "#roomId_#{roomIdLength}"
+    else
+      $ '<input>',
+        id: "roomId_#{roomIdLength}"
+        class: 'roomId'
+        type: 'text'
+      .appendTo "#roomId_#{roomIdLength}"
+    addRemoveButton("#{roomIdLength}")
+    addBrTag()
+
+  saveRoomIds = ->
+    roomIds = []
+    $('#roomIds').children('div').each (i) ->
+      v = $(@).children('input').val()
+      if !isNaN(v)
+        roomIds["#{i}"] = v
+    chrome.storage.local.set {rids: roomIds.join(',')}
+
+  $(document).on 'click', '#add', ->
+    addRoomId()
+
+  $(document).on 'click', '.delete', ->
+    roomIdLength = $(@).data().id
+    $("#roomId_#{roomIdLength}").remove()
+    saveRoomIds()
+
+  $(document).on 'click', '#save', ->
+    saveRoomIds()
+
+  chrome.storage.local.get (item) ->
+    rids = item.rids.split(',')
+    if rids
+      for rid in rids
+        addRoomId rid
+  addBrTag()
+  addAddButton()
+  addSaveButton()
--- src/js/options.js
+++ src/js/options.js
@@ -1,14 +1,94 @@
 $(function() {
+  var addAddButton, addBrTag, addRemoveButton, addRoomId, addSaveButton, saveRoomIds;
+  addBrTag = function() {
+    var roomIdLength;
+    roomIdLength = $('.roomId').length;
+    return $('<br>').appendTo(`#roomId_${roomIdLength}`);
+  };
+  addAddButton = function() {
+    return $('<input>', {
+      id: 'add',
+      type: 'button',
+      value: 'Add'
+    }).appendTo('#mainSetting');
+  };
+  addSaveButton = function() {
+    return $('<input>', {
+      id: 'save',
+      type: 'button',
+      value: 'Save'
+    }).appendTo('#mainSetting');
+  };
+  addRemoveButton = function(roomIdLength) {
+    return $('<input>', {
+      class: 'delete',
+      type: 'button',
+      "data-id": roomIdLength,
+      value: 'Delete'
+    }).appendTo(`#roomId_${roomIdLength}`);
+  };
+  addRoomId = function(rid) {
+    var roomIdLength;
+    roomIdLength = $('.roomId').length;
+    $('<div>', {
+      id: `roomId_${roomIdLength}`,
+      class: 'roomId'
+    }).appendTo('#roomIds');
+    if (rid) {
+      $('<input>', {
+        class: `roomId ${rid}`,
+        type: 'text',
+        value: rid
+      }).appendTo(`#roomId_${roomIdLength}`);
+    } else {
+      $('<input>', {
+        id: `roomId_${roomIdLength}`,
+        class: 'roomId',
+        type: 'text'
+      }).appendTo(`#roomId_${roomIdLength}`);
+    }
+    addRemoveButton(`${roomIdLength}`);
+    return addBrTag();
+  };
+  saveRoomIds = function() {
+    var roomIds;
+    roomIds = [];
+    $('#roomIds').children('div').each(function(i) {
+      var v;
+      v = $(this).children('input').val();
+      if (!isNaN(v)) {
+        return roomIds[`${i}`] = v;
+      }
+    });
+    return chrome.storage.local.set({
+      rids: roomIds.join(',')
+    });
+  };
+  $(document).on('click', '#add', function() {
+    return addRoomId();
+  });
+  $(document).on('click', '.delete', function() {
+    var roomIdLength;
+    roomIdLength = $(this).data().id;
+    $(`#roomId_${roomIdLength}`).remove();
+    return saveRoomIds();
+  });
+  $(document).on('click', '#save', function() {
+    return saveRoomIds();
+  });
   chrome.storage.local.get(function(item) {
-    if (item.rids) {
-      return $('#rids').val(item.rids);
+    var j, len, results, rid, rids;
+    rids = item.rids.split(',');
+    if (rids) {
+      results = [];
+      for (j = 0, len = rids.length; j < len; j++) {
+        rid = rids[j];
+        results.push(addRoomId(rid));
+      }
+      return results;
     }
   });
-  return $('#save').on('click', function() {
-    return chrome.storage.local.set({
-      rids: $('#rids').val()
-    }, function() {
-      return console.log('save?');
-    });
-  });
+  addBrTag();
+  addAddButton();
+  return addSaveButton();
 });
